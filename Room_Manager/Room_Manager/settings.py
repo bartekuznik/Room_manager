ec143de603b60e24567c542fe666046ae127bc21
Foxxich <64288356+Foxxich@users.noreply.github.com>
2024-01-08 22:15:05 +0100

    solved requests getting problem

--- Room_Manager/Room_Manager/settings.py
+++ Room_Manager/Room_Manager/settings.py
@@ -137,3 +137,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1']
--- Room_Manager/app3/urls.py
+++ Room_Manager/app3/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import GetAvailableServerView, ManageServerView, ReplaceServerView
 
 urlpatterns = [
-path('status/', GetAvailableServerView.as_view(), name='get_available_server'),
+    path('status/', GetAvailableServerView.as_view(), name='get_available_server'),
     path('manage_server/', ManageServerView.as_view(), name='manage_server'),
     path('replace_server/', ReplaceServerView.as_view(), name='replace_server'),
 ]
